4f4ad57e1c23f1d829ff2d4562d3080992c776ae
Ryan Tracey <ryan.m.tracey@gmail.com>
2017-08-20 16:22:35 -0700

    $1

--- insertion-sort/index.js
+++ insertion-sort/index.js
@@ -1,25 +1,33 @@
-const insertionSort = function(list) {
-    let switchIndex;
+class InsertionSort {
+    static sort(list) {
+        this.list = list;
+        let compareIndex;
     
-    for (let i = 0; i < list.length; i++) {
-        let requiresSwitch = false;
-        let switchIndex = i + 1;
-        let nextIndex = i;
+        for (let i = 0; i <= this.list.length; i++) {
+            let requiresSwitch = false;
+            let compareIndex = i + 1;
+            let nextIndex = i;
 
-        while (list[switchIndex] < list[nextIndex] && nextIndex > 0) {
-            nextIndex--;
-            requiresSwitch = true;
+            while (this.list[compareIndex] < this.list[nextIndex] && nextIndex >= 0) {
+                nextIndex--;
+                requiresSwitch = true;
+            }
+
+            if (requiresSwitch) {
+                let insertIndex = nextIndex < 0 ? 0 : nextIndex + 1;
+                let temp = this.list[insertIndex];
+                this._insert(insertIndex, temp, compareIndex, this.list[compareIndex]);
+            }
         }
 
-        if (requiresSwitch) {
-            let targetIndex = nextIndex < 0 ? 0 : nextIndex;
-            let temp = list[targetIndex];
-            list[targetIndex] = list[switchIndex];
-            list[switchIndex] = temp;
-        }
+        console.log(this.list);
+        return this.list;
     }
 
-    console.log(list);
+    static _insert(insertIndex, insertItem, compareIndex, switchItem) {
+        this.list.splice(compareIndex, 1)
+        this.list.splice(insertIndex, 0, switchItem);
+    }
 };
 
-insertionSort([4, 2, 5, 2, 1, 9, 7, 10]);
+InsertionSort.sort([10, 0, 3, 2, 1, 0, 2, 7, 2, 1]);
